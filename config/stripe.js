d32cea0daf326a0a1d468615d52c2210bc437372
robksawyer <robksawyer@gmail.com>
2014-11-26 21:17:53 -0800

    Minor tweaks

--- config/stripe.js
+++ config/stripe.js
@@ -11,20 +11,9 @@ module.exports.stripe = {
 
   /***************************************************************************
   *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
+  * Your Stripe secret key.                                                  *
   *                                                                          *
   ***************************************************************************/
-   connection: 'localDiskDb',
-
-  /***************************************************************************
-  *                                                                          *
-  * How and whether Sails will attempt to automatically rebuild the          *
-  * tables/collections/etc. in your schema.                                  *
-  *                                                                          *
-  * See http://sailsjs.org/#/documentation/concepts/ORM/model-settings.html  *
-  *                                                                          *
-  ***************************************************************************/
-   migrate: 'alter'
+   secretKey: process.env.STRIPE_SECRET_KEY,
 
 };
