f95b91ee62469a844efe8256388fdf1c9b00dd6a
robksawyer <robksawyer@gmail.com>
2014-12-02 02:05:59 -0800

    Working on task #11

--- api/controllers/CashController.js
+++ api/controllers/CashController.js
@@ -6,57 +6,116 @@
  */
 
 var secrets = require('../../config/stripe'),
-    stripe = require("stripe")(secrets.stripe.secretKey);
+    applicationFeeInPercent = process.env.MINNOW_APPLICATION_FEE_PERCENT || 10,
+    stripe = require("stripe")(secrets.stripe.secretKey),
+    ErrorService = require('../services/ErrorService'),
+    MailerService = require('../services/MailerService');
 
 module.exports = {
 
+  /**
+  *
+  * Handles making the secret transaction.
+  * More info: https://stripe.com/docs/api#transfers and https://stripe.com/docs/tutorials/sending-transfers
+  * 
+  * Here’s what the flow looks like:
+  * 1. A transfer is created via the API. At this point, the transfer’s status is pending.
+  * 2. You receive a transfer.paid webhook when the transfer is expected to be available in the recipient’s bank account or debit card. However, this 
+  *    webhook does not guarantee that the transfer was actually successful.
+  * 3. If the transfer fails, you’ll receive a transfer.failed webhook within five business days (that’s unfortunately how long some banks take to 
+  *    return transfers) and the transfer will be marked as failed.
+  * 4. You can safely assume the transfer was successful if you don’t receive a transfer.failed webhook within five business days.
+  * 
+  * Timelines: 
+  * Unlike charging a credit card, sending a transfer is not synchronous. For bank accounts, transfers will be available in the bank account the next 
+  * business day if created before 21:00 UTC (2pm PST). If the transfer fails (due to a typo in the bank details, for example), it can take up to five 
+  * business days for Stripe to be notified.
+  * 
+  * Transfers to debit cards can take 1 to 2 days to complete. However, unlike with bank accounts, we'll know instantaneously if the debit card is not 
+  * valid when it is added to the recipient.
+  **/
   transfer: function(req, res){
     var params = req.params.all(),
-        transferAmount = Math.round(Number(params.amount) + 50);
+        chargeAmount = Math.round(Number(params.amount)),
+        appProfitAmount = (chargeAmount * applicationFeeInPercent/100),
+        userProfitAmount = chargeAmount - appProfitAmount;
 
-    console.log(transferAmount);
+    console.log("Charge Amt: " + chargeAmount);
+    console.log("App Profit: " + appProfitAmount);
+    console.log("User Profit: " + userProfitAmount);
 
     User.findOneByEmail(req.user.email, function(err, sender){
+      if(err){
+        console.log(err);
+        ErrorService.makeErrorResponse(500, {
+          message: "Something went wrong", 
+          type: "error", 
+          options: {}
+        }, res, req);
+      }
+
       User.findOneByEmail(params.reciever, function (err, reciever){
-        stripe.transfers.create({
-          amount: transferAmount, // amount in cents
+        console.log(err);
+        ErrorService.makeErrorResponse(500, {
+          message: "Something went wrong", 
+          type: "error", 
+          options: {}
+        }, res, req);
+
+        //The Minnow transaction fee
+        stripe.charges.create({
+
+          amount: appProfitAmount, // amount in cents
           currency: "usd",
           recipient: 'self',
           card: sender.recipient.default_card,
-          statement_description: 'Transfer to: ' + params.reciever
+          statement_description: 'Secret transfer fee.'
+
         }, function(err, fromSender) {
           if(err){
             console.log(err);
-            req.flash("message", '<div class="alert alert-danger">Something went wrong</div>');
-            res.cookie("message", {message: "Something went wrong", type: "error", options: {}});
-            res.redirect('/user');
+            ErrorService.makeErrorResponse(500, {
+              message: "Something went wrong", 
+              type: "error", 
+              options: {}
+            }, res, req);
           } else {
             console.log(fromSender);
+
+            //If the receiver exists
             if(reciever){
 
-              stripe.transfers.create({
-                amount: params.amount, // amount in cents
+              //The secret owner's cut
+              stripe.charges.create({
+
+                amount: userProfitAmount, // amount in cents
                 currency: "usd",
                 recipient: reciever.recipient.id,
                 card: reciever.recipient.default_card,
-                statement_description: 'Transfer to: ' + params.reciever
+                statement_description: 'A secret.'
+
               }, function(err, toReciever) {
                 if(err){
                   console.log(err);
-                  req.flash("message", '<div class="alert alert-danger">Something went wrong</div>');
-                  res.cookie("message", {message: "Something went wrong", type: "error", options: {}});
-                  res.redirect('/user');
+                  ErrorService.makeErrorResponse(500, {
+                    message: "Something went wrong", 
+                    type: "error", 
+                    options: {}
+                  }, res, req);
                 } else {
                   console.log(toReciever);
-                  req.flash("message", '<div class="alert alert-danger">You\re Transfer Should Finish in 2 Days</div>');
-                  res.cookie("message", {message: "Success", type: "Success", options: {}});
-                  res.redirect('/user');
+                  res.json(200, {
+                        message: "Success! Your transaction will be completed within 2 days.", 
+                        type: "Success", 
+                        options: {}
+                      }
+                    );
                 }
               });
 
             } else {
-              console.log('set up email to send about transfer');
-              MailerService.send({
+              console.log('the receiver did not exist, that is weird');
+              /*MailerService.send({
                 from:       process.env.REPLY_TO_EMAIL_ADDRESS,
                 to:         user.email,
                 replyTo:    process.env.FROM_EMAIL_ADDRESS,
@@ -64,15 +123,24 @@ module.exports = {
                 html:       '<h3>Thanks for signing up</h3><p><a href="http://localhost:1337/user/' + user.id + '/activate/' + user.activationToken + '">Please Activate Your Account</a></p>'
               }, function(err, response){
                 sails.log.debug('nodemailer sent', err, response);
-              });
-              req.flash("message", '<div class="alert alert-danger">We Sent an Email asking them to accept your cash</div>');
-              res.cookie("message", {message: "Success", type: "Success", options: {}});
-              res.redirect('/user');
+              });*/
+              ErrorService.makeErrorResponse(500, {
+                    message: "Something went wrong", 
+                    type: "error", 
+                    options: {}
+                  }, res, req);
             }
+
           }
         });
       });
     });
+  },
+
+  handleTransaction: function(recipient, ){
+
+  }
+
 
   }
 
