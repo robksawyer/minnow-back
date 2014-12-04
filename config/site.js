f8331c4e2b94cfdd911484b4b046702f69201528
robksawyer <robksawyer@gmail.com>
2014-12-04 01:10:15 -0800

    Added some new configuration files

---
+++ config/site.js
@@ -0,0 +1,24 @@
+/**
+*
+* site.js
+* Global site configuration file.
+* 
+**/
+
+module.exports.site = {
+  
+  /**
+  *
+  * Default number of Posts to return to the views
+  *
+  **/
+  posts_return_limit: 10,
+
+  /**
+  *
+  * Max number of Posts to ever return to the views
+  *
+  **/
+  posts_return_max: 100
+
+}
