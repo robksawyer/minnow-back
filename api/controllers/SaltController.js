81f65bbb848a9ca6edccfc727bd121a7b6d0177f
robksawyer <robksawyer@gmail.com>
2014-12-11 00:52:23 -0800

    Working on #7

---
+++ api/controllers/SaltController.js
@@ -0,0 +1,11 @@
+/**
+ * SaltController
+ *
+ * @description :: Server-side logic for managing Salts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
