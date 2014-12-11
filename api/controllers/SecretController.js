11731b1a54e79528fe0628470eb84ef39cb85ee8
robksawyer <robksawyer@gmail.com>
2014-12-10 16:50:46 -0800

    Working on the method getSecretForPost in SecretController.

--- api/controllers/SecretController.js
+++ api/controllers/SecretController.js
@@ -14,20 +14,57 @@ module.exports = {
      * @param   {{}}        where   Used query conditions
      * @param   {Function}  next    Callback function to call after query
      */
-    getSecretForPost: function(postId, next){
-        //
-        //Check to see if the user is the owner or has purchased the secret
-        //
-        Secret
-            .find({post: postId})
-            .exec(function(err, secret){
-                if (err) {
-                    sails.log.error("[Failed to fetch the secret data]");
-                    sails.log.error(err);
+    getSecretForPost: function(req, res){
+        if(!req.param('post')) {
+            ErrorService.makeErrorResponse(403, 'You did not provide a post id.', req, res);
+        }
+        var postId = req.param('post');
+        //Check to see if the post exists
+        Post
+            .find({id: postId})
+            .exec(function(err, post){
+                if(err){
+                    ErrorService.makeErrorResponse(500, 'There was an error retrieving the post.', req, res);
+                }
+                if(!post){
+                    ErrorService.makeErrorResponse(404, 'This post does not exist.', req, res);
+                }
+                
+                sails.log(post);
+
+                var hasAccess = false;
+                if(req.session.user.id === post[0].owner){
+                    hasAccess = true;
+                } else if(req.session.user.id){
+                    //Look up the user's purchases to see if they have purchased the secret
+                    Purchase.find({user: req.session.user.id, post: post[0].id})
+                        .exec(function(err, purchase){
+                            if(err){
+                                ErrorService.makeErrorResponse(500, 'There was an error retrieving the purchase.', req, res);
+                            }
+                            sails.log(purchase);
+                            if(purchase){
+                                hasAccess = true;
+                            }
+
+                        });
+                    
+                }
+                sails.log("hasAccess: " + hasAccess);
+                if(hasAccess === true){
+                    Secret
+                        .find({post: postId})
+                        .exec(function(err, secret){
+                            if (err) {
+                                ErrorService.makeErrorResponse(500, 'There was an error retrieving the secret.', req, res);
+                            }
+                            ResponseService.makeResponse(secret, null, req, res);
+                        });
                 }
 
-                next(err, secret);
             });
+
+        res.forbidden(err);
     }
 
 };
